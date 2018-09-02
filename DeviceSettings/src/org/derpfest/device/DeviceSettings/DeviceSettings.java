930480a2e3245942a444b2102ee5644e492d56e1
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-09-02 12:46:45 +0200

    sm8150-common: DeviceSettings: Use local FileUtils
    Change-Id: I5e06015c8ea65399bb3643183dd17641ca0d3ad7

--- DeviceSettings/src/org/derpfest/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/derpfest/device/DeviceSettings/DeviceSettings.java
@@ -43,7 +43,7 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.TwoStatePreference;
 
-import com.android.internal.util.derp.FileUtils;
+import org.derpfest.device.DeviceSettings.FileUtils;
 import org.derpfest.device.DeviceSettings.Constants;
 
 public class DeviceSettings extends PreferenceFragment
