f6e9c2be2663d0923951508503336e36c9066345
Anirudh Gupta <anirudhgupta109@gmail.com>
2019-12-10 17:36:23 +0000

    sm8150-common: Introduce DeviceSettings
    Taken from: https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/tree/99e6831dc41b8b19552ef1f6ee59db7d1e0105a4/DeviceSettings
    
    Edits after:
     - Adapted to AndroidX
     - Includes all translations from pie cycle
     - Remove DisplayModes and Dolby
     - Add DC-Dimming, FPS Info tile, Refresh Rate preferences
     - Change layout to a cleaner look
     - Nuke KeyHandler
     - Change KeyHandler class and lib overlays to DeviceSettings
     - Make DC-Dimming and HBM QS tile reflect active status
    
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- common.mk
+++ common.mk
@@ -88,6 +88,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     AntHalService
 
+# AOSiP Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
@@ -461,7 +465,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Update engine
