8bcd2e566e1c856398881984cfe6641d794171bd
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-02-01 19:35:22 +0530

    sm8150-common: DeviceSettings: Add Touchscreen Gesture controls
    Imported from LineagePart, adapted to LineageHW implementation present in fwb instead of lineage-sdk
    
    Change-Id: I08064ca6c7a7e58783847677843522b818c583de
    TouchscreenGestureSettings class written by:
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>

--- DeviceSettings/src/org/derpfest/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/org/derpfest/device/DeviceSettings/Startup.java
@@ -25,7 +25,12 @@ import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.PreferenceManager;
 
+import org.derpfest.device.DeviceSettings.TouchscreenGestureSettings;
+
 public class Startup extends BroadcastReceiver {
+
+    private static final String TAG = "BootReceiver";
+    private static final String ONE_TIME_TUNABLE_RESTORE = "hardware_tunable_restored";
 
     private void restore(String file, boolean enabled) {
         if (file == null) {
@@ -46,6 +51,17 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
         boolean enabled = false;
+        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    private boolean hasRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
+    }
+
+    private void setRestoredTunable(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }
