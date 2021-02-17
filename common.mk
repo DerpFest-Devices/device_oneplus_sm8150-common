5fe68bf82a426a3243143d9eb7a832f350a858ba
NurKeinNeid <mralexman3000@gmail.com>
2021-02-17 17:46:52 +0100

    sm8150-common: Add FOD animations
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -260,6 +260,9 @@ PRODUCT_PACKAGES += \
     vendor.oneplus.fingerprint.extension@1.0.vendor \
     vendor.oneplus.hardware.display@1.0.vendor
 
+# FOD
+EXTRA_FOD_ANIMATIONS := true
+
 # For config.fs
 PRODUCT_PACKAGES += \
     fs_config_files
