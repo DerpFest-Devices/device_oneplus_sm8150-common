StatiXOS/android_device_oneplus_sm8150-common

sm8150-common: DeviceSettings: Update HBM tile & switch on screen off
What happens is that HBM is being turned off in lower level and the node is not being updated.
Writing 0 to node when the screen turns off to go around that.

Service only runs when HBM is turned on.
it stops itself when the screen turns off / HBM is turned off

Also remove some useless code all around and slightly improve...

Change-Id: I058df7a014a7b7676a7cec71a9a29177b71c088b

DeviceSettings/src/com/statix/device/DeviceSettings/DeviceSettings.java
@@ -97,7 +97,7 @@ public class DeviceSettings extends PreferenceFragment
         mHBMModeSwitch = (TwoStatePreference) findPreference(KEY_HBM_SWITCH);
         mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
         mHBMModeSwitch.setChecked(HBMModeSwitch.isCurrentlyEnabled(this.getContext()));
-        mHBMModeSwitch.setOnPreferenceChangeListener(new HBMModeSwitch());
+        mHBMModeSwitch.setOnPreferenceChangeListener(this);
 
         // Refresh Rate
         if (getResources().getBoolean(R.bool.config_deviceHasHighRefreshRate)) {
@@ -118,6 +118,12 @@ public class DeviceSettings extends PreferenceFragment
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mHBMModeSwitch.setChecked(HBMModeSwitch.isCurrentlyEnabled(this.getContext()));
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
          if (preference == mAutoRefreshRate) {
             Boolean enabled = (Boolean) newValue;
@@ -131,6 +137,16 @@ public class DeviceSettings extends PreferenceFragment
         } else if (preference == mRefreshRate) {
             Boolean enabled = (Boolean) newValue;
             RefreshRateSwitch.setPeakRefresh(getContext(), enabled);
+        } else if (preference == mHBMModeSwitch) {
+            Boolean enabled = (Boolean) newValue;
+            Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "5" : "0");
+            Intent hbmIntent = new Intent(this.getContext(),
+                    com.statix.device.DeviceSettings.HBMModeService.class);
+            if (enabled) {
+                this.getContext().startService(hbmIntent);
+            } else {
+                this.getContext().stopService(hbmIntent);
+            }
         } else {
             Constants.setPreferenceInt(getContext(), preference.getKey(),
                     Integer.parseInt((String) newValue));

DeviceSettings/src/com/statix/device/DeviceSettings/HBMModeService.java
@@ -1,0 +1,57 @@
+/*
+* Copyright (C) 2020 Yet Another AOSP Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+import com.statix.device.DeviceSettings.HBMModeSwitch;
+
+public class HBMModeService extends Service {
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                Utils.writeValue(HBMModeSwitch.getFile(), "0");
+                stopSelf();
+            }
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mReceiver, intentFilter);
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}

DeviceSettings/src/com/statix/device/DeviceSettings/HBMModeTileService.java
@@ -18,15 +18,19 @@
 package com.statix.device.DeviceSettings;
 
 import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import androidx.preference.PreferenceManager;
 
+import java.lang.IllegalArgumentException;
+
 @TargetApi(24)
 public class HBMModeTileService extends TileService {
-    private boolean enabled = false;
+
+    private Intent mHbmIntent;
 
     @Override
     public void onDestroy() {
@@ -40,16 +44,14 @@ public class HBMModeTileService extends TileService {
 
     @Override
     public void onTileRemoved() {
+        tryStopService();
         super.onTileRemoved();
     }
 
     @Override
     public void onStartListening() {
         super.onStartListening();
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
-        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        getQsTile().updateTile();
+        updateState();
     }
 
     @Override
@@ -60,12 +62,27 @@ public class HBMModeTileService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        // NOTE: reverse logic, enabled reflects the state before press
         Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
-        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_HBM_SWITCH, enabled ? false : true).commit();
-        //getQsTile().setLabel(enabled ? "HBM off" : "HBM On");
-        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        if (!enabled) {
+            mHbmIntent = new Intent(this,
+                    com.statix.device.DeviceSettings.HBMModeService.class);
+            this.startService(mHbmIntent);
+        }
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        if (!enabled) tryStopService();
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         getQsTile().updateTile();
+    }
+
+    private void tryStopService() {
+        if (mHbmIntent == null) return;
+        this.stopService(mHbmIntent);
+        mHbmIntent = null;
     }
 }

DerpFest-Devices/device_oneplus_sm8150-common

DeviceSettings/src/com/derp/device/DeviceSettings/DeviceSettings.java
@@ -116,7 +116,7 @@ public class DeviceSettings extends PreferenceFragment
         mHBMModeSwitch = (TwoStatePreference) findPreference(KEY_HBM_SWITCH);
         mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
         mHBMModeSwitch.setChecked(HBMModeSwitch.isCurrentlyEnabled(this.getContext()));
-        mHBMModeSwitch.setOnPreferenceChangeListener(new HBMModeSwitch());
+        mHBMModeSwitch.setOnPreferenceChangeListener(this);
 
         if (getResources().getBoolean(R.bool.config_deviceHasHighRefreshRate)) {
             boolean autoRefresh = AutoRefreshRateSwitch.isCurrentlyEnabled(this.getContext());
@@ -149,6 +149,12 @@ public class DeviceSettings extends PreferenceFragment
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mHBMModeSwitch.setChecked(HBMModeSwitch.isCurrentlyEnabled(this.getContext()));
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mFpsInfo) {
             boolean enabled = (Boolean) newValue;
@@ -176,6 +182,16 @@ public class DeviceSettings extends PreferenceFragment
         } else if (preference == mRefreshRate) {
             Boolean enabled = (Boolean) newValue;
             RefreshRateSwitch.setPeakRefresh(getContext(), enabled);
+        } else if (preference == mHBMModeSwitch) {
+            Boolean enabled = (Boolean) newValue;
+            Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "5" : "0");
+            Intent hbmIntent = new Intent(this.getContext(),
+                    com.derp.device.DeviceSettings.HBMModeService.class);
+            if (enabled) {
+                this.getContext().startService(hbmIntent);
+            } else {
+                this.getContext().stopService(hbmIntent);
+            }
         } else {
             Constants.setPreferenceInt(getContext(), preference.getKey(),
                     Integer.parseInt((String) newValue));

DeviceSettings/src/com/derp/device/DeviceSettings/HBMModeService.java
@@ -1,0 +1,57 @@
+/*
+* Copyright (C) 2020 Yet Another AOSP Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.derp.device.DeviceSettings;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+import com.derp.device.DeviceSettings.HBMModeSwitch;
+
+public class HBMModeService extends Service {
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_SCREEN_OFF)) {
+                Utils.writeValue(HBMModeSwitch.getFile(), "0");
+                stopSelf();
+            }
+        }
+    };
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mReceiver, intentFilter);
+        return START_REDELIVER_INTENT;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
+    @Override
+    public void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}

DeviceSettings/src/com/derp/device/DeviceSettings/HBMModeTileService.java
@@ -18,15 +18,19 @@
 package com.derp.device.DeviceSettings;
 
 import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 import androidx.preference.PreferenceManager;
 
+import java.lang.IllegalArgumentException;
+
 @TargetApi(24)
 public class HBMModeTileService extends TileService {
-    private boolean enabled = false;
+
+    private Intent mHbmIntent;
 
     @Override
     public void onDestroy() {
@@ -40,16 +44,14 @@ public class HBMModeTileService extends TileService {
 
     @Override
     public void onTileRemoved() {
+        tryStopService();
         super.onTileRemoved();
     }
 
     @Override
     public void onStartListening() {
         super.onStartListening();
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
-        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
-        getQsTile().updateTile();
+        updateState();
     }
 
     @Override
@@ -60,12 +62,27 @@ public class HBMModeTileService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
-        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        // NOTE: reverse logic, enabled reflects the state before press
         Utils.writeValue(HBMModeSwitch.getFile(), enabled ? "0" : "5");
-        sharedPrefs.edit().putBoolean(DeviceSettings.KEY_HBM_SWITCH, enabled ? false : true).commit();
-        //getQsTile().setLabel(enabled ? "HBM off" : "HBM On");
-        getQsTile().setState(enabled ? Tile.STATE_INACTIVE : Tile.STATE_ACTIVE);
+        if (!enabled) {
+            mHbmIntent = new Intent(this,
+                    com.derp.device.DeviceSettings.HBMModeService.class);
+            this.startService(mHbmIntent);
+        }
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = HBMModeSwitch.isCurrentlyEnabled(this);
+        if (!enabled) tryStopService();
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
         getQsTile().updateTile();
+    }
+
+    private void tryStopService() {
+        if (mHbmIntent == null) return;
+        this.stopService(mHbmIntent);
+        mHbmIntent = null;
     }
 }