cebd3d6bedf754b95a7b73ce25c0c12c4b21e566
NurKeinNeid <mralexman3000@gmail.com>
2021-04-02 12:32:22 +0530

    sm8150-common: Derp it
    Change-Id: I824468386dca2707e20b141f74ac65102ba6cf45
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2021 DerpFest
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,7 +17,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-derp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
