b7e86f93dd1a427f3ef88467afdd88f61a77d854
Ido Ben-Hur <idoybh2@gmail.com>
2021-04-15 15:33:28 +0300

    sm8150-common: DeviceSettings: KeyHandler: Vibrate when switching to and from total silence
    total silence zen mode blocks vibrations, going around that.

--- DeviceSettings/src/org/derpfest/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/org/derpfest/device/DeviceSettings/KeyHandler.java
@@ -76,6 +76,7 @@ public class KeyHandler implements DeviceKeyHandler {
     WakeLock mProximityWakeLock;
     WakeLock mGestureWakeLock;
     private int mProximityTimeOut;
+    private int mPrevKeyCode = 0;
     private boolean mProximityWakeSupported;
 
     public KeyHandler(Context context) {
@@ -118,9 +119,12 @@ public class KeyHandler implements DeviceKeyHandler {
             return null;
         }
 
-        mAudioManager.setRingerModeInternal(sSupportedSliderRingModes.get(keyCodeValue));
-        mNotificationManager.setZenMode(sSupportedSliderZenModes.get(keyCodeValue), null, TAG);
         doHapticFeedback(sSupportedSliderHaptics.get(keyCodeValue));
+        mAudioManager.setRingerModeInternal(sSupportedSliderRingModes.get(keyCodeValue));
+        if (mPrevKeyCode == Constants.KEY_VALUE_TOTAL_SILENCE)
+            doHapticFeedback(sSupportedSliderHaptics.get(keyCodeValue));
+        mNotificationManager.setZenMode(sSupportedSliderZenModes.get(keyCodeValue), null, TAG);
+        mPrevKeyCode = keyCodeValue;
         int position = scanCode == 601 ? 2 : scanCode == 602 ? 1 : 0;
         sendUpdateBroadcast(position);
         return null;
