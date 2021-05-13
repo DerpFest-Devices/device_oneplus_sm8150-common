a0d478860d0894d079cfce21f841a1cee6cd0b75
NurKeinNeid <mralexman3000@gmail.com>
2021-05-13 17:26:32 +0200

    Revert "sm8150-common: Enable Project ID Quota support on userdata."
    This reverts commit d6433aad6361e075cf9d02d2bc9e111928dea0a0.

--- common.mk
+++ common.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 
-# Project ID Quota
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
