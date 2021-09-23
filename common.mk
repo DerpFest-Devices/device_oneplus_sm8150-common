59750c514bf515312cdbe60927e2c79377a41b7d
Pranav <npv12@iitbbs.ac.in>
2021-09-23 16:09:40 +0530

    sm8150-common: Don't build vendor.oneplus.hardware.CameraMDMHIDL@1.0.so
    * It was prebuilt anyway
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -161,7 +161,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     Snap \
     vendor.oneplus.hardware.camera@1.0.vendor:64 \
-    vendor.oneplus.hardware.CameraMDMHIDL@1.0.vendor:64 \
     vendor.qti.hardware.camera.device@1.0.vendor:64
 
 # Common init scripts
