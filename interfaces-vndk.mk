48f06f07791966430f6e0985669ddb656e3a5ac3
Pranav <npv12@iitbbs.ac.in>
2021-10-16 20:45:30 +0530

    sm8150-common: Arrange vndk interfaces
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- interfaces-vndk.mk
+++ interfaces-vndk.mk
@@ -1,21 +1,53 @@
+# Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor \
-	android.hardware.drm@1.4.vendor \
+    android.hardware.bluetooth@1.1.vendor
+
+# DRM
+PRODUCT_PACKAGES += \
+	android.hardware.drm@1.4.vendor
+
+# Keymaster
+PRODUCT_PACKAGES += \
 	android.hardware.gatekeeper@1.0.vendor \
-    android.hardware.keymaster@4.1.vendor \
-	android.system.net.netd@1.1.vendor \
-	android.hardware.neuralnetworks@1.3.vendor \
+    android.hardware.keymaster@4.1.vendor
+
+# Location
+PRODUCT_PACKAGES += \
+	android.hardware.gnss@1.0.vendor \
+	android.hardware.gnss@2.1.vendor
+
+# Net
+PRODUCT_PACKAGES += \
+	android.system.net.netd@1.1.vendor 
+
+# Neural networks
+PRODUCT_PACKAGES += \
+	android.hardware.neuralnetworks@1.3.vendor
+
+# Power
+PRODUCT_PACKAGES += \
+	android.hardware.power@1.2.vendor
+
+# RIL
+PRODUCT_PACKAGES += \
 	android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-	android.frameworks.sensorservice@1.0.vendor \
-	android.hardware.wifi@1.5.vendor \
-	android.hardware.gnss@1.0.vendor \
-	android.hardware.gnss@2.1.vendor \
+
+# Secure Element
+PRODUCT_PACKAGES += \
 	android.hardware.secure_element@1.1.vendor \
-	android.hardware.secure_element@1.2.vendor \
-	android.hardware.power@1.2.vendor \
-	android.hardware.audio@4.0.vendor:64
+	android.hardware.secure_element@1.2.vendor
+
+# Sensor
+PRODUCT_PACKAGES += \
+	android.hardware.audio@4.0.vendor:64 \
+	android.frameworks.sensorservice@1.0.vendor
+
+# Wifi
+PRODUCT_PACKAGES += \
+	android.hardware.wifi@1.5.vendor
+	
 	
