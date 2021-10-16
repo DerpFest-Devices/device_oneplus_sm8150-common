cabeb98a5cd998dfe6f412b7aa2e368f109bbbc6
John Galt <johngaltfirstrun@gmail.com>
2021-10-16 21:12:28 +0530

    sm8150-common: Switch to AOSP bt
    This reverts commit e1fddf0cb0604b92cf09f3dc1ed15fbb3dd52437.
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    Revert "sm8150-common: Support A2DP offload for Bluetooth Audio HAL V2"
    
    This reverts commit 0d2bf3269fa835dfda30a1684c1dbb7991c10bf6.
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    Revert "sm8150-common: Switch to QTI bt stack"
    
    This reverts commit 5758f73e29ede2a086dcea012459345d4615f3ff.
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    Revert "sm8150-common: AAC frame control enabled for hal implementation."
    
    This reverts commit 8dc28758b940bd52beaf7e50c084d55dacd638fa.
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    
    Add necessary bluetooth audio packages/manifest entry
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.1-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.r_submix.default \
@@ -129,11 +128,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
 # Boot control
 PRODUCT_PACKAGES += \
@@ -150,6 +149,8 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     liba2dpoffload \
     libbthost_if \
+    android.hardware.bluetooth.audio@2.0 \
+    android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
