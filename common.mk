eb4b508f30cdde11747b27a63949ce5432b1c8a5
John Galt <johngaltfirstrun@gmail.com>
2021-10-16 20:34:07 +0530

    sm8150-common: Add back  vendor.oneplus.hardware.display
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- common.mk
+++ common.mk
@@ -249,7 +249,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus \
+    vendor.oneplus.hardware.display@1.0.vendor
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
