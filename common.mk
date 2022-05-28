c6faa36fb52db7ecc2b505d3531f11ad578290d9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:27:37 +0200

    sm8150-common: common: Drop atrace HIDL
    Change-Id: Iaaaf62e9dda26dd15d51f5975d9f21fe1037d2eb
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -27,10 +27,6 @@ PRODUCT_PACKAGES += \
     KeyHandler \
     tri-state-key-calibrate
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     acdbdata_adsp_avs_config.bin_symlink \
