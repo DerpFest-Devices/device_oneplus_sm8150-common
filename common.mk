48706694e1e9760df1d64255f422c9fbfca476a6
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-15 22:53:20 +0100

    sm8150-common: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- common.mk
+++ common.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
