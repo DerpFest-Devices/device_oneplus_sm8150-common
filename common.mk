b518cb6d73440f067fa337b6a722f1e838b8e219
LuK1337 <priv.luk@gmail.com>
2024-12-05 16:41:51 +0100

    Revert "sm8150-common: Build 64-bit only libvndfwk_detect_jni.qti.vendor"
    Before:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x0
    
    After:
    audio_hw_extn: spkr_prot_feature_init: Called with feature Enabled, vendor_enhanced_info 0x3
    
    This reverts commit ef6e06eb92de78c5809c54bc4c16757478ccf5cd.
    
    Change-Id: Ie5c33eafc1e0be2eeda8e0a6bbbc3712e32986e9

--- common.mk
+++ common.mk
@@ -271,7 +271,7 @@ PRODUCT_COPY_FILES += \
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
-    libvndfwk_detect_jni.qti.vendor:64 # Needed by CNE app
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
